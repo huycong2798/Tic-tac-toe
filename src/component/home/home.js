7242cb43d01db1d3c41f10e85551288cf0ba7de6
unknown <huycong2798@gmail.com>
2019-11-06 20:52:10 +0700

    fix css button at home route

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -2,17 +2,23 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "antd/dist/antd.css";
 import {Card, Button} from "antd";
+import "../../containers/home/home.css";
 
 class HomeComponent extends React.PureComponent {
   render() {
     return (
       <div>
+        <img
+          src="https://taigamehayfree.info/wp-content/uploads/2018/01/co-caro-icon.png"
+          alt="caro"
+          style={{width: "30%", height: "30%"}}
+        />
         <Card>
           <Link to="/offline">
-            <Button>PVE</Button>
+            <Button className="home-button">PVE</Button>
           </Link>
           <Link to="/online">
-            <Button>PVP</Button>
+            <Button className="home-button">PVP</Button>
           </Link>
         </Card>
       </div>
